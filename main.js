298bdbc2505e9f8838bf4aacfe39f1cd61db89cb
Chatt <magnus.chatt@gmail.com>
2021-03-03 19:15:39 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var pattern = new RegExp("^(nano|xrb)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$");
+
+function main() {
+    $("#deposit_address_view").hide()
+    var urlParams = new URLSearchParams(window.location.search);
+    var address = urlParams.get('address');
+    var isValid = pattern.test(address)
+    console.log(isValid)
+    if (isValid) {
+
+    } else {
+        $("#deposit_address_view").show()
+    }
+}
+
+function addressContinue() {
+    address = $("#address_textarea").val()
+    window.location.href = "?address=" + address
+}
+
+function pasteAddress() {
+    navigator.clipboard.readText()
+    .then(text => {
+        $("#address_textarea").val(text);
+        addressChanged()
+    })
+    .catch(err => {
+        console.error('Failed to read clipboard contents: ', err);
+    });
+}
+
+function addressChanged() {
+    var textarea = $("#address_textarea").get(0)
+    address = textarea.value.toLowerCase()
+    address = address.replace(/[^1-9a-z_]/g, '')
+    textarea.value = address
+    var isValid = pattern.test(address)
+    $("#address_continue").get(0).disabled = !isValid
+}

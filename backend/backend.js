ac7e64f8b8c93a7a204bc47ac16f633f19cbb5d9
Magnus Vinther <magnus@uber.com>
2021-11-28 21:01:40 +0100

    currency / rates endpoint, like a cached version of exchangerate.api

--- backend/backend.js
+++ backend/backend.js
@@ -1,21 +1,80 @@
 const http = require('http');
-const url = require("url");
+const https = require('https');
+const url = require('url');
+const currencies = require('./currencies.json');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'application/json');
-    let respJson = '{}';
+let exchangeRates = null;
+let exchangeRatesLastRefresh = new Date(0);
 
-    let path = url.parse(req.url).pathname;
-    if (path === "/currency/rates" && req.method === "GET") {
-        res.statusCode = 200;
-        respJson = '{"hello": "world"}';
+function errorResponse(res) {
+    res.statusCode = 500;
+    res.end('{}\n');
+}
+
+function currencyRatesResponse(res, resObj) {
+    res.statusCode = 200;
+    let jsonString = JSON.stringify(resObj);
+    res.end(jsonString);
+}
+
+function getCurrencyRates(_, inRes) {
+
+    const now = new Date();
+    const hoursSince = Math.abs(exchangeRatesLastRefresh - now) / (60*60*1000);
+    if (hoursSince < 1) {
+        currencyRatesResponse(inRes, exchangeRates)
+        return;
     }
 
-    res.end(respJson + '\n');
+    console.log("refreshing FIAT exchange rates")
+    const options = {
+        hostname: 'api.exchangerate.host',
+        path: '/latest?base=USD',
+    }
+
+    https.get(options, res => {
+        let json = '';
+        res.on('data', function (chunk) {
+            json += chunk;
+        });
+
+        res.on('end', function () {
+            if (res.statusCode === 200) {
+                try {
+                    inRes.statusCode = 200;
+                    exchangeRates = JSON.parse(json);
+                    exchangeRatesLastRefresh = new Date();
+                    currencyRatesResponse(inRes, exchangeRates);
+                } catch (e) {
+                    console.log('Error parsing JSON!' + e);
+                    errorResponse(inRes);
+                }
+            } else {
+                console.log('Status:', res.statusCode);
+                errorResponse(inRes);
+            }
+        });
+
+    }).on('error', function (err) {
+        console.log('Error:', err);
+        errorResponse(inRes);
+    });
+}
+
+const server = http.createServer((req, res) => {
+    let path = url.parse(req.url).pathname;
+    res.setHeader('Content-Type', 'application/json');
+
+    if (path === "/currency/rates" && req.method === "GET") {
+        getCurrencyRates(req, res);
+        return;
+    }
+
+    res.statusCode = 404;
+    res.end('{}\n');
 });
 
 server.listen(port, hostname, () => {

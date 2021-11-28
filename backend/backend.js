76a2d5e441bd5c82e8f17d3bab1f538a984f7584
Magnus Vinther <magnus@uber.com>
2021-11-28 08:31:49 +0100

    simple hello world backend

--- backend/backend.js
+++ backend/backend.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const url = require("url");
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    let respJson = '{}';
+
+    let path = url.parse(req.url).pathname;
+    if (path === "/currency/rates" && req.method === "GET") {
+        res.statusCode = 200;
+        respJson = '{"hello": "world"}';
+    }
+
+    res.end(respJson + '\n');
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`AcceptNano backend running at http://${hostname}:${port}/`);
 });
